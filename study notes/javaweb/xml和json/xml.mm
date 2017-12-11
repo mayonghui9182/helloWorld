da939d60ac933a7f605442dcecd6c6519b0ea8b1
mayonghui <984478628@qq.com>
2017-12-11 21:26:42 +0800

    update

--- study notes/javaweb/xml和json/xml.mm
+++ study notes/javaweb/xml和json/xml.mm
@@ -1,6 +1,6 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1511669383237" ID="ID_243902934" MODIFIED="1511670533687" STYLE="fork" TEXT="xml">
+<node CREATED="1511669383237" ID="ID_243902934" MODIFIED="1512302487241" STYLE="fork" TEXT="xml">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -101,7 +101,7 @@
 </node>
 <node CREATED="1511669787018" ID="ID_590207127" MODIFIED="1511670533703" STYLE="fork" TEXT="&#x6ce8;&#x91ca;"/>
 <node CREATED="1511669787018" ID="ID_19307905" MODIFIED="1511670533703" STYLE="fork" TEXT="&#x8f6c;&#x4e49;&#x5b57;&#x7b26;"/>
-<node CREATED="1511669787018" ID="ID_1662543205" MODIFIED="1512139436624" STYLE="fork" TEXT="CDATA&#x533a;">
+<node CREATED="1511669787018" ID="ID_1662543205" MODIFIED="1512302487264" STYLE="fork" TEXT="CDATA&#x533a;">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -123,8 +123,7 @@
       ]]&gt;
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1511669787034" ID="ID_1440188162" MODIFIED="1511682278315" STYLE="fork" TEXT="&#x5904;&#x7406;&#x6307;&#x4ee4;&#xff08;processing instruction&#xff09;">
 <richcontent TYPE="NOTE"><html>
@@ -149,6 +148,93 @@
     </p>
   </body>
 </html></richcontent>
+</node>
+</node>
+<node CREATED="1512302512546" ID="ID_719497603" MODIFIED="1512918666255" POSITION="left" TEXT="xml&#x7ea6;&#x675f;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      schema&#21442;&#32771;&#65306;http://blog.51cto.com/wyait/1917046
+    </p>
+    <p>
+      http://www.cnblogs.com/chnking/archive/2006/07/12/448849.html
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1512302525328" ID="ID_1512475323" MODIFIED="1512302532803" TEXT="dtd&#x7ea6;&#x675f;">
+<node CREATED="1512302643593" ID="ID_827100087" MODIFIED="1512303430576" TEXT="&#x5f15;&#x5165;&#x7ea6;&#x675f;&#x6587;&#x4ef6;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &lt;!DOCTYPE rootName system &quot;filePath&quot;&gt;
+    </p>
+    <p>
+      &lt;!DOCTYPE rootName public &quot;dtd&#21517;&#31216;&quot; &quot;dtd&#25991;&#20214;url&quot;&gt;
+    </p>
+    <p>
+      rootNma&#65306;&#26681;&#20803;&#32032;&#25991;&#20214;&#21517;
+    </p>
+    <p>
+      location&#65306;&#32422;&#26463;&#25991;&#20214;&#20301;&#32622;&#12290;system&#25351;&#25991;&#20214;&#22312;&#26412;&#22320;&#65292;public&#25351;&#25991;&#20214;&#26469;&#33258;&#32593;&#32476;
+    </p>
+    <p>
+      filePath&#65306;&#32422;&#26463;&#25991;&#20214;&#36335;&#24452;
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      &#22312;xml&#20869;&#37096;&#23450;&#20041;&#32422;&#26463;
+    </p>
+    <p>
+      &lt;!DOCTYPE rootName&#160;&#160;[&#32422;&#26463;&#20869;&#23481;]&gt;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1512302533217" ID="ID_921618755" MODIFIED="1512309545544" TEXT="schema&#x7ea6;&#x675f;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul class="list-paddingleft-2" style="font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, &#x5fae;&#x8f6f;&#x96c5;&#x9ed1;, Arial, sans-serif; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 20px; font-size: 16px; color: rgb(51, 51, 51); font-style: normal; font-weight: normal; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); list-style-type: disc">
+      <li style="font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, &#x5fae;&#x8f6f;&#x96c5;&#x9ed1;, Arial, sans-serif; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; font-size: 16px">
+        <p style="font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, &#x5fae;&#x8f6f;&#x96c5;&#x9ed1;, Arial, sans-serif; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; font-size: 16px">
+          targetNamespace&#160;<font face="&#x5b8b;&#x4f53;">&#23646;&#24615;</font>&#160;<font face="&#x5b8b;&#x4f53;">&#32473;&#24403;&#21069;&#30340;</font>Schem<font face="&#x5b8b;&#x4f53;">&#25991;&#20214;&#36215;&#21517;&#65292;&#21363;&#21517;&#31216;&#31354;&#38388;</font>
+        </p>
+      </li>
+      <li style="font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, &#x5fae;&#x8f6f;&#x96c5;&#x9ed1;, Arial, sans-serif; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; font-size: 16px">
+        <font face="Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, &#x5fae;&#x8f6f;&#x96c5;&#x9ed1;, Arial, sans-serif">elementFormDefault</font><font face="&#x5b8b;&#x4f53;">&#20803;&#32032;&#29992;&#20110;&#25351;&#23450;&#23616;&#37096;&#20803;&#32032;&#26159;&#21542;&#21463;&#21040;&#35813;</font><font face="Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, &#x5fae;&#x8f6f;&#x96c5;&#x9ed1;, Arial, sans-serif">schema</font><font face="&#x5b8b;&#x4f53;">&#25351;&#23450;</font><font face="Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, &#x5fae;&#x8f6f;&#x96c5;&#x9ed1;, Arial, sans-serif">targetNamespace</font><font face="&#x5b8b;&#x4f53;">&#25152;&#25351;&#23450;&#30340;&#21517;&#31216;&#31354;&#38388;&#38480;&#23450;</font>
+      </li>
+      <li style="font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, &#x5fae;&#x8f6f;&#x96c5;&#x9ed1;, Arial, sans-serif; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; font-size: 16px">
+        <font face="Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, &#x5fae;&#x8f6f;&#x96c5;&#x9ed1;, Arial, sans-serif">attributeFormDefault</font><font face="&#x5b8b;&#x4f53;">&#20803;&#32032;&#29992;&#20110;&#25351;&#23450;&#23616;&#37096;&#23646;&#24615;&#26159;&#21542;&#21463;&#21040;&#35813;</font><font face="Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, &#x5fae;&#x8f6f;&#x96c5;&#x9ed1;, Arial, sans-serif">schema</font><font face="&#x5b8b;&#x4f53;">&#25351;&#23450;</font><font face="Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, &#x5fae;&#x8f6f;&#x96c5;&#x9ed1;, Arial, sans-serif">targetNamespace</font><font face="&#x5b8b;&#x4f53;">&#25152;&#25351;&#23450;&#30340;&#21517;&#31216;&#31354;&#38388;&#38480;&#23450;</font>
+      </li>
+      <li style="font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, &#x5fae;&#x8f6f;&#x96c5;&#x9ed1;, Arial, sans-serif; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; font-size: 16px">
+        <p style="font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, &#x5fae;&#x8f6f;&#x96c5;&#x9ed1;, Arial, sans-serif; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; font-size: 16px">
+          <font face="&#x5b8b;&#x4f53;">&#22312;</font>xml<font face="&#x5b8b;&#x4f53;">&#25991;&#20214;&#20013;</font>&#160;<font face="&#x5b8b;&#x4f53;">&#38656;&#35201;</font>&#160;<font face="&#x5b8b;&#x4f53;">&#36890;&#36807;</font>&#160;xmlns&#160;<font face="&#x5b8b;&#x4f53;">&#26469;&#24341;&#20837;&#19981;&#21516;&#21517;&#31216;&#19979;&#30340;</font>Schema<font face="&#x5b8b;&#x4f53;">&#25991;&#20214;&#12290;</font>
+        </p>
+      </li>
+      <li style="font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, &#x5fae;&#x8f6f;&#x96c5;&#x9ed1;, Arial, sans-serif; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; font-size: 16px">
+        <p style="font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, &#x5fae;&#x8f6f;&#x96c5;&#x9ed1;, Arial, sans-serif; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; font-size: 16px">
+          <font face="&#x5b8b;&#x4f53;">&#22914;&#26524;&#22312;</font>xml<font face="&#x5b8b;&#x4f53;">&#25991;&#20214;&#20351;&#29992;&#20102;&#22810;&#20010;</font>&#160;xmlns<font face="&#x5b8b;&#x4f53;">&#24341;&#20837;&#22810;&#20010;&#21517;&#31216;&#31354;&#38388;&#26102;&#38656;&#35201;&#22312;</font>xmlns<font face="&#x5b8b;&#x4f53;">&#21518;&#38754;&#20351;&#29992;&#20882;&#21495;&#32473;&#24403;&#21069;&#30340;&#21517;&#31216;&#31354;&#38388;&#36215;&#21035;&#21517;&#21517;&#12290;&#36890;&#36807;&#36825;&#20010;&#21035;&#21517;&#21306;&#20998;&#21040;&#24213;&#24403;&#21069;</font>xml<font face="&#x5b8b;&#x4f53;">&#20013;&#30340;&#26631;&#31614;&#21463;&#38480;&#20110;&#20855;&#20307;&#21738;&#20010;</font>Schema<font face="&#x5b8b;&#x4f53;">&#25991;&#20214;&#12290;</font>
+        </p>
+      </li>
+      <li style="font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, &#x5fae;&#x8f6f;&#x96c5;&#x9ed1;, Arial, sans-serif; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; font-size: 16px">
+        &#36890;&#36807;xsi:schemaLocation&#25351;&#23450;&#21629;&#21517;&#31354;&#38388;&#25351;&#21521;&#30340;&#32422;&#26463;&#25991;&#20214;&#30340;&#30340;&#20855;&#20307;&#20301;&#32622;
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node CREATED="1512305964917" ID="ID_1000570026" MODIFIED="1512305968545" TEXT="&#x540d;&#x79f0;&#x7a7a;&#x95f4;"/>
 </node>
 </node>
 </node>
--- study notes/框架/Hibernate/hibernate.mm
+++ study notes/框架/Hibernate/hibernate.mm
@@ -1,7 +1,7 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1510645979658" ID="ID_425023716" MODIFIED="1510645984134" TEXT="hibernate">
-<node CREATED="1510645985124" ID="ID_301222527" MODIFIED="1510646249324" POSITION="right" TEXT="&#x6620;&#x5c04;&#x5173;&#x7cfb;">
+<node CREATED="1510645979658" ID="ID_425023716" MODIFIED="1512884483326" TEXT="hibernate">
+<node CREATED="1510645985124" ID="ID_301222527" MODIFIED="1512918672458" POSITION="right" TEXT="&#x6620;&#x5c04;&#x5173;&#x7cfb;">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -13,7 +13,109 @@
   </body>
 </html>
 </richcontent>
-<node CREATED="1510645994425" ID="ID_516324088" MODIFIED="1510646090601" TEXT="&#x4e00;&#x5bf9;&#x4e00;&#x6620;&#x5c04;">
+<node CREATED="1512884489979" ID="ID_735237219" MODIFIED="1512884499281" TEXT="&#x4e3b;&#x952e;&#x6620;&#x5c04;">
+<node CREATED="1512884517679" ID="ID_1409874876" MODIFIED="1512918672505" TEXT="assign">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      assign&#65306;&#25351;&#23450;
+    </p>
+    <p>
+      &#30001;&#22806;&#37096;&#31243;&#24207;&#25351;&#23450;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1512887925928" ID="ID_374840311" MODIFIED="1512918672520" TEXT="increment">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      hibernate&#35843;&#29992;incrementgenerator.generate()&#26041;&#27861;&#65292;&#20351;&#29992;select max(id)&#26041;&#27861;&#33719;&#21462;&#20027;&#38190;&#26368;&#22823;&#20540;&#65288;&#27599;&#20010;session&#21482;&#21462;&#19968;&#27425;&#65289;&#65292;&#20197;&#35813;&#20540;&#20026;&#22522;&#30784;&#65292;&#27599;&#27425;&#21152;&#19968;&#65292;&#22312;&#20869;&#23384;&#20013;&#29983;&#25104;&#20027;&#38190;&#65292;&#19981;&#20381;&#36182;&#19982;&#24213;&#23618;&#25968;&#25454;&#24211;&#65292;&#21487;&#20197;&#36328;&#25968;&#25454;&#24211;&#12290;
+    </p>
+    <p>
+      &#35813;&#26041;&#27861;&#20026;synchronize&#65292;&#25152;&#20197;&#22312;&#19968;&#20010;jvm&#20869;&#37096;&#27809;&#38382;&#39064;&#65292;&#20294;&#26159;&#24403;&#22810;&#20010;jvm&#21516;&#26102;&#24182;&#21457;&#35775;&#38382;&#25968;&#25454;&#24211;&#26102;&#65292;&#23601;&#20250;&#25253;&#38169;&#12290;
+    </p>
+    <p>
+      &#25152;&#20197;&#19981;&#36866;&#21512;&#38598;&#32676;&#29615;&#22659;&#12290;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1512887843956" ID="ID_1909420517" MODIFIED="1512918672552" TEXT="sequence">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#20351;&#29992;&#25968;&#25454;&#24211;sequence&#65292;&#19981;&#25351;&#23450;sequence&#21517;&#31216;&#26102;&#65292;&#20351;&#29992;hibernate&#40664;&#35748;&#30340;hibernate_sequence&#65292;&#21069;&#25552;&#26159;&#21019;&#24314;&#20102;&#35813;sequence&#12290;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1512887849644" ID="ID_1628377485" MODIFIED="1512918672567" TEXT="identity">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#26377;&#25968;&#25454;&#24211;&#33258;&#24049;&#29983;&#20135;&#65292;&#20294;&#26159;&#20027;&#38190;&#24517;&#39035;&#20026;&#33258;&#22686;&#38271;&#31867;&#22411;&#12290;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1512884512347" ID="ID_1865500268" MODIFIED="1512918672583" TEXT="native">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      hibernate&#26681;&#25454;&#25968;&#25454;&#24211;&#33258;&#21160;&#21028;&#26029;&#20351;&#29992;&#21738;&#31181;:hilo&#65292;identity&#65292;sequence&#31561;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1512889353266" ID="ID_1553209403" MODIFIED="1512918672599" TEXT="foreign">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#20351;&#29992;&#21478;&#19968;&#20010;&#34920;&#30340;&#20027;&#38190;&#26469;&#20316;&#20026;&#35813;&#23545;&#35937;&#30340;&#20027;&#38190;&#12290;&#27880;&#24847;&#29992;&#20110;&#19968;&#23545;&#19968;&#20851;&#31995;&#12290;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1512887690569" ID="ID_1452061821" MODIFIED="1512919261317" TEXT="&#x7c7b;&#x578b;&#x6620;&#x5c04;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      https://www.cnblogs.com/aijava/archive/2008/06/04/2191859.html
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1510645994425" ID="ID_516324088" MODIFIED="1512918672645" TEXT="&#x4e00;&#x5bf9;&#x4e00;&#x6620;&#x5c04;">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -25,7 +127,7 @@
   </body>
 </html>
 </richcontent>
-<node CREATED="1510646094671" ID="ID_1045452556" MODIFIED="1510646811156" TEXT="&#x5916;&#x952e;&#x6620;&#x5c04;">
+<node CREATED="1510646094671" ID="ID_1045452556" MODIFIED="1512918672661" TEXT="&#x5916;&#x952e;&#x6620;&#x5c04;">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -37,13 +139,13 @@
   </body>
 </html>
 </richcontent>
-<node CREATED="1510646142692" ID="ID_1982486028" MODIFIED="1510646203889" TEXT="&#x5916;&#x952e;&#x65b9;">
+<node CREATED="1510646142692" ID="ID_1982486028" MODIFIED="1512918672708" TEXT="&#x5916;&#x952e;&#x65b9;">
 <richcontent TYPE="NOTE"><html>
   <head>
     
   </head>
   <body>
-    <pre style="margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; white-space: pre-wrap; font-family: Courier New !important; font-size: 12px; color: rgb(0, 0, 0); font-style: normal; font-weight: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; word-spacing: 0px"><font face="Courier New !important" size="12px !important" color="rgb(0, 0, 255)">&lt;</font><font face="Courier New !important" size="12px !important" color="rgb(128, 0, 0)">many-to-one </font><font face="Courier New !important" size="12px !important" color="rgb(255, 0, 0)">name</font><font face="Courier New !important" size="12px !important" color="rgb(0, 0, 255)">=&quot;users1&quot;</font><font face="Courier New !important" size="12px !important" color="rgb(255, 0, 0)"> class</font><font face="Courier New !important" size="12px !important" color="rgb(0, 0, 255)">=&quot;Users1&quot;</font><font face="Courier New !important" size="12px !important" color="rgb(255, 0, 0)"> column</font><font face="Courier New !important" size="12px !important" color="rgb(0, 0, 255)">=&quot;RESUSERSID&quot;</font><font face="Courier New !important" size="12px !important" color="rgb(255, 0, 0)"> cascade</font><font face="Courier New !important" size="12px !important" color="rgb(0, 0, 255)">=&quot;all&quot;</font><font face="Courier New !important" size="12px !important" color="rgb(255, 0, 0)"> unique</font><font face="Courier New !important" size="12px !important" color="rgb(0, 0, 255)">=&quot;true&quot;&gt;&lt;/</font><font face="Courier New !important" size="12px !important" color="rgb(128, 0, 0)">many-to-one</font><font face="Courier New !important" size="12px !important" color="rgb(0, 0, 255)">&gt;</font></pre>
+    <pre style="margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; white-space: pre-wrap; font-family: Courier New !important; font-size: 12px; color: rgb(0, 0, 0); font-style: normal; font-weight: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; word-spacing: 0px"><font color="rgb(0, 0, 255)" face="Courier New !important">&lt;</font><font color="rgb(128, 0, 0)" face="Courier New !important">many-to-one </font><font color="rgb(255, 0, 0)" face="Courier New !important">name</font><font color="rgb(0, 0, 255)" face="Courier New !important">=&quot;users1&quot;</font><font color="rgb(255, 0, 0)" face="Courier New !important"> class</font><font color="rgb(0, 0, 255)" face="Courier New !important">=&quot;Users1&quot;</font><font color="rgb(255, 0, 0)" face="Courier New !important"> column</font><font color="rgb(0, 0, 255)" face="Courier New !important">=&quot;RESUSERSID&quot;</font><font color="rgb(255, 0, 0)" face="Courier New !important"> cascade</font><font color="rgb(0, 0, 255)" face="Courier New !important">=&quot;all&quot;</font><font color="rgb(255, 0, 0)" face="Courier New !important"> unique</font><font color="rgb(0, 0, 255)" face="Courier New !important">=&quot;true&quot;&gt;&lt;/</font><font color="rgb(128, 0, 0)" face="Courier New !important">many-to-one</font><font color="rgb(0, 0, 255)" face="Courier New !important">&gt;</font></pre>
     <p>
       &#20351;&#29992;&lt;many-to-one unique='true'&gt;
     </p>
@@ -51,7 +153,7 @@
 </html>
 </richcontent>
 </node>
-<node CREATED="1510646146381" ID="ID_689534849" MODIFIED="1510646595604" TEXT="&#x4e3b;&#x952e;&#x65b9;">
+<node CREATED="1510646146381" ID="ID_689534849" MODIFIED="1512918672739" TEXT="&#x4e3b;&#x952e;&#x65b9;">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -64,7 +166,7 @@
 </richcontent>
 </node>
 </node>
-<node CREATED="1510646099562" ID="ID_1864069213" MODIFIED="1510646806742" TEXT="&#x4e3b;&#x952e;&#x6620;&#x5c04;">
+<node CREATED="1510646099562" ID="ID_1864069213" MODIFIED="1512918672755" TEXT="&#x4e3b;&#x952e;&#x6620;&#x5c04;">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -76,18 +178,18 @@
   </body>
 </html>
 </richcontent>
-<node CREATED="1510646757068" ID="ID_1086435128" MODIFIED="1510646796284" TEXT="&#x4e3b;&#x952e;&#x65b9;">
+<node CREATED="1510646757068" ID="ID_1086435128" MODIFIED="1512918672786" TEXT="&#x4e3b;&#x952e;&#x65b9;">
 <richcontent TYPE="NOTE"><html>
   <head>
     
   </head>
   <body>
-    <pre style="margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; white-space: pre-wrap; font-family: Courier New !important; font-size: 12px; color: rgb(0, 0, 0); font-style: normal; font-weight: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; word-spacing: 0px"><font face="Courier New !important" size="12px !important" color="rgb(0, 0, 255)">&lt;</font><font face="Courier New !important" size="12px !important" color="rgb(128, 0, 0)">one-to-one </font><font face="Courier New !important" size="12px !important" color="rgb(255, 0, 0)">name</font><font face="Courier New !important" size="12px !important" color="rgb(0, 0, 255)">=&quot;users1&quot;</font><font face="Courier New !important" size="12px !important" color="rgb(255, 0, 0)"> class</font><font face="Courier New !important" size="12px !important" color="rgb(0, 0, 255)">=&quot;Users2&quot;</font><font face="Courier New !important" size="12px !important" color="rgb(255, 0, 0)"> cascade</font><font face="Courier New !important" size="12px !important" color="rgb(0, 0, 255)">=&quot;all&quot;</font> <font face="Courier New !important" size="12px !important" color="rgb(0, 0, 255)">&gt;&lt;/</font><font face="Courier New !important" size="12px !important" color="rgb(128, 0, 0)">one-to-one</font><font face="Courier New !important" size="12px !important" color="rgb(0, 0, 255)">&gt;</font></pre>
+    <pre style="margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; white-space: pre-wrap; font-family: Courier New !important; font-size: 12px; color: rgb(0, 0, 0); font-style: normal; font-weight: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; word-spacing: 0px"><font color="rgb(0, 0, 255)" face="Courier New !important">&lt;</font><font color="rgb(128, 0, 0)" face="Courier New !important">one-to-one </font><font color="rgb(255, 0, 0)" face="Courier New !important">name</font><font color="rgb(0, 0, 255)" face="Courier New !important">=&quot;users1&quot;</font><font color="rgb(255, 0, 0)" face="Courier New !important"> class</font><font color="rgb(0, 0, 255)" face="Courier New !important">=&quot;Users2&quot;</font><font color="rgb(255, 0, 0)" face="Courier New !important"> cascade</font><font color="rgb(0, 0, 255)" face="Courier New !important">=&quot;all&quot;</font> <font color="rgb(0, 0, 255)" face="Courier New !important">&gt;&lt;/</font><font color="rgb(128, 0, 0)" face="Courier New !important">one-to-one</font><font color="rgb(0, 0, 255)" face="Courier New !important">&gt;</font></pre>
   </body>
 </html>
 </richcontent>
 </node>
-<node CREATED="1510646763850" ID="ID_732232372" MODIFIED="1510647110132" TEXT="&#x5916;&#x952e;&#x65b9;">
+<node CREATED="1510646763850" ID="ID_732232372" MODIFIED="1512918672817" TEXT="&#x5916;&#x952e;&#x65b9;">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -123,7 +225,7 @@
 </node>
 </node>
 <node CREATED="1510647196401" ID="ID_951229255" MODIFIED="1510647209278" TEXT="&#x7ec4;&#x4ef6;&#x6620;&#x5c04;">
-<node CREATED="1510647443359" ID="ID_1985178532" MODIFIED="1510647549911" TEXT="&#x591a;">
+<node CREATED="1510647443359" ID="ID_1985178532" MODIFIED="1512918672849" TEXT="&#x591a;">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -137,7 +239,7 @@
 </html>
 </richcontent>
 </node>
-<node CREATED="1510647446397" ID="ID_1922153775" MODIFIED="1510647663711" TEXT="&#x4e00;">
+<node CREATED="1510647446397" ID="ID_1922153775" MODIFIED="1512918672880" TEXT="&#x4e00;">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -170,5 +272,64 @@
 <node CREATED="1510646001035" ID="ID_1047691491" MODIFIED="1510646004989" TEXT="&#x4e00;&#x5bf9;&#x591a;&#x6620;&#x5c04;"/>
 <node CREATED="1510646005246" ID="ID_1775229728" MODIFIED="1510646010268" TEXT="&#x591a;&#x5bf9;&#x591a;&#x6620;&#x5c04;"/>
 </node>
+<node CREATED="1512887705684" ID="ID_465744302" MODIFIED="1512887711407" POSITION="right" TEXT="&#x5bf9;&#x8c61;&#x64cd;&#x4f5c;"/>
+<node CREATED="1512891958436" ID="ID_1501330481" MODIFIED="1512891963247" POSITION="right" TEXT="&#x4e09;&#x79cd;&#x72b6;&#x6001;">
+<node CREATED="1512892070585" ID="ID_1874991758" MODIFIED="1512918665927" TEXT="unsave-value">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      hibernate&#26144;&#23556;&#25991;&#20214;id&#26631;&#31614;&#30340;&#23646;&#24615;unsave-Value&#40664;&#35748;&#20026;null&#12290;
+    </p>
+    <p>
+      hibernate&#20250;&#26681;&#25454;&#23427;&#26469;&#21028;&#26029;&#19968;&#20010;&#23545;&#35937;&#26159;&#30636;&#26102;&#24577;&#36824;&#26159;&#28216;&#31163;&#24577;&#65292;&#22914;&#26524;&#30456;&#31561;&#20026;&#30636;&#26102;&#24577;&#65292;&#25191;&#34892;save&#65292;&#22914;&#26524;&#19981;&#30456;&#31561;&#20026;&#28216;&#31163;&#24577;&#65292;&#25191;&#34892;update&#12290;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1512891969581" ID="ID_74520320" MODIFIED="1512918672911" TEXT="&#x77ac;&#x65f6;&#x6001;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      id&#20026;null&#65292;&#22312;session&#20013;&#26080;&#32531;&#20914;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1512891981254" ID="ID_218313751" MODIFIED="1512918672927" TEXT="&#x6301;&#x4e45;&#x6001;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      id&#19981;&#20026;null&#65292;&#22312;session&#20013;&#26377;&#32531;&#23384;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1512891978307" ID="ID_1594294162" MODIFIED="1512918672942" TEXT="&#x6e38;&#x79bb;&#x6001;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      id&#19981;&#20026;null&#65292;&#22312;session&#20013;&#26080;&#32531;&#23384;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1512884487310" ID="ID_1886150848" MODIFIED="1512884487310" POSITION="left" TEXT=""/>
 </node>
 </map>
