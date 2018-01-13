d5156320a77688265736cae82ad9e6329b1c83a9
mayonghui <984478628@qq.com>
2018-01-13 22:54:34 +0800

    update

--- sourceCode/testMaven/src/main/java/com/ma/test/testHibernate/testHibernate.java
+++ sourceCode/testMaven/src/main/java/com/ma/test/testHibernate/testHibernate.java
@@ -2,6 +2,7 @@ package com.ma.test.testHibernate;
 
 import java.util.LinkedHashMap;
 import org.apache.xmlbeans.impl.jam.internal.elements.VoidClassImpl;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.classloading.internal.ClassLoaderServiceImpl;
@@ -9,14 +10,17 @@ import org.hibernate.cfg.Configuration;
 
 public class testHibernate {
 	public static void main(String[] args) {
-		SessionFactory SessionFactory = new Configuration().configure().buildSessionFactory();
-		Session session = SessionFactory.openSession();
-		TestTab testTab = new TestTab();
-		testTab.setField1("field1");
-		testTab.setField2("field2");
-		testTab.setField3("field3");
-		session.save(testTab);//find(TestTab.class);
-		session.close();
-		
+		try {
+			SessionFactory SessionFactory = new Configuration().configure().buildSessionFactory();
+			Session session = SessionFactory.openSession();
+			TestTab testTab = new TestTab();
+			testTab.setField1("field1");
+			testTab.setField2("field2");
+			testTab.setField3("field3");
+			session.save(testTab);//find(TestTab.class);
+			session.close();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		}
 	}
 }
